28800ea59fb320acc6df94f9465afb4db93c368f
Luis Ricardo <luiscastrodev@gmail.com>
2024-01-30 17:31:28 -0300

    Configuracao hilt e timber

--- app/src/main/java/br/com/movieapp/MainActivity.kt
+++ app/src/main/java/br/com/movieapp/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.movieapp.ui.theme.MovieAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MovieAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
